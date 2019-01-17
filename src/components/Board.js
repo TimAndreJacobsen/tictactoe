2a794924901028ff5a16bced00e9bfc1e737a65f
Tim Andre Jacobsen <35067641+TimAndreJacobsen@users.noreply.github.com>
2019-01-17 12:31:13 +0100

    moved calculateWinner() %% handleClick() to Game.JS

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,18 +2,6 @@ import React from 'react';
 
 class Board extends React.Component {
 
-    handleClick(i) {
-        const squares = this.props.squares.slice(); // slice returns a copy of original squares array
-        if (calculateWinner(squares) || squares[i]){
-            return;
-        }
-        squares[i] = this.props.xIsNext ? 'X' : 'O'; // change the state of [i](clicked square) X or O dependant on bool flag status
-        this.setState({
-            squares: squares, // write state to board.squares
-            xIsNext: !this.props.xIsNext, // flips the flag
-        }); 
-        
-    }
 
     renderSquare(i) {
         return (
@@ -57,25 +45,5 @@ function Square(props) {
         </button>
     );
 }
-
-function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
 
 export default Board;
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Board from './Board';
 
+
 class Game extends React.Component {
     constructor(props){
         super(props);
@@ -10,6 +11,23 @@ class Game extends React.Component {
             }],
             xIsNext: true,
         }
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]){
+            return; // if game won || square.value != null => do nothing
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'; // change the state of [i](clicked square) X or O dependant on bool flag status
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext, // flips the flag
+        }); 
+        
     }
 
     render() {
@@ -29,7 +47,7 @@ class Game extends React.Component {
           <div className="game-board">
             <Board
                 squares={current.squares}
-                onClick={(i) => this.handleClick(i)} 
+                onClick={(i) => this.handleClick(i)}
             />
           </div>
           <div className="game-info">
@@ -39,6 +57,26 @@ class Game extends React.Component {
         </div>
       );
     }
+}
+
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
   }
 
 export default Game;
